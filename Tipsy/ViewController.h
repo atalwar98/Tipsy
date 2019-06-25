718e88bacd29341becf2f92de02874d7b213c5f9
Anika Talwar <atalwar98@fb.com>
2019-06-25 14:03:53 -0700

    Initial Commit

---
+++ Tipsy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipsy
+//
+//  Created by atalwar98 on 6/25/19.
+//  Copyright © 2019 atalwar98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tipsy/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tipsy
+//
+//  Created by atalwar98 on 6/25/19.
+//  Copyright © 2019 atalwar98. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
