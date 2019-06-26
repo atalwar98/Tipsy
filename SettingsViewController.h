cd9522d23c8b6ecf72e56a5330d0a23ac00be938
Anika Talwar <atalwar98@fb.com>
2019-06-25 23:16:59 -0700

    adding preliminary bonus for settings

---
+++ SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  Tipsy
+//
+//  Created by atalwar98 on 6/25/19.
+//  Copyright © 2019 atalwar98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
